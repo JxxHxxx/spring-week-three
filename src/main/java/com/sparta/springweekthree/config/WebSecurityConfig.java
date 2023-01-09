f989656de1f966a500659ae78d15ce0fada0561c
JxxHxxx <leesin5498@naver.com>
2023-01-09 18:22:09 +0900

    FEAT #1 Jwt 인증/인가에 맞게 설정 변경
    게시글 조회/로그인, 회원가입 필터 예외 대상

--- src/main/java/com/sparta/springweekthree/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/springweekthree/config/WebSecurityConfig.java
@@ -1,40 +1,98 @@
 package com.sparta.springweekthree.config;
 
+import com.sparta.springweekthree.jwt.JwtAuthFilter;
+import com.sparta.springweekthree.jwt.JwtUtil;
+import com.sparta.springweekthree.security.MemberDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig  {
+
+    private final MemberDetailsServiceImpl memberDetailsService;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-
-        return (web -> web.ignoring()
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
                 .requestMatchers(PathRequest.toH2Console())
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.csrf().disable();
 
-        http.authorizeRequests().antMatchers("/auth/**").permitAll()
-                                .anyRequest().authenticated();
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        //로그인 사용
-        http.formLogin();
+        http.authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/bulletin-boards/**").permitAll() // 게시글 조회는 인가 X
+                .anyRequest().authenticated()
+                // JWT 인증/인가를 사용하기 위한 설정 이건 모든 URI가 다돌아.
+                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+
+//        http.formLogin().loginProcessingUrl("auth/login/v2");
 
         return http.build();
     }
-
-
 }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//
+//        return (web -> web.ignoring()
+//                .requestMatchers(PathRequest.toH2Console())
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
+//    }
+//
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//
+//        // JWT 방식 설정
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        http.authorizeRequests().antMatchers("/auth/**").permitAll()
+//                                .anyRequest().authenticated()
+//                                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+//
+//        //로그인 사용
+//        http.formLogin().loginProcessingUrl("auth/login/v2");
+
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+
+//        http.addFilterBefore(new CustomSecurityFilter(memberDetailsService, passwordEncoder()), UsernamePasswordAuthenticationFilter.class);
+//                .antMatcher("/auto/login/v2");
