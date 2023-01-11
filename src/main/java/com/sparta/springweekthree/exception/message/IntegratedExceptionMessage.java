4b498c59ccdf4181ba006ba4ade1709f5cd681c5
JxxHxxx <leesin5498@naver.com>
2023-01-11 21:16:29 +0900

    FEAT #1 통합 에러 메시지 상수

---
+++ src/main/java/com/sparta/springweekthree/exception/message/IntegratedExceptionMessage.java
@@ -0,0 +1,19 @@
+package com.sparta.springweekthree.exception.message;
+
+import lombok.Getter;
+
+@Getter
+public enum IntegratedExceptionMessage {
+
+    NOT_EXISTED_BULLETIN_BOARD("게시글이 존재하지 않습니다."),
+    DELETED_BULLETIN_BOARD("삭제된 게시글입니다."),
+    ILLEGAL_ACCESS_UPDATE_OR_DELETE("작성자 혹은 관리자만 수정/삭제 할 수 있습니다.");
+
+    IntegratedExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    private final String message;
+
+
+}
